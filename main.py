f503de91b12c62521064b376c4d519d25b526711
Alessia Manna <alessia.00m@gmail.com>
2021-12-09 11:41:02 +0100

    aggiunta cartella coni

--- main.py
+++ main.py
@@ -9,17 +9,21 @@ import PIL.ImageOps
 def main():
     image_list = []
     for filename in glob.glob("coni/*.jpg"):
-        img = Image.open(filename)
-        image_list.append(img)
+        image_list.append(filename)
 
-    for x in image_list:
-        new_image = transforms.adjust_brightness(x, 1.5)
+    if not os.path.exists('images'):
+        os.mkdir('images')
+
+    for image in image_list:
+        img = Image.open(image)
+        new_image = transforms.adjust_brightness(img, 1.5)
         new_image = transforms.adjust_contrast(new_image, 10)
         new_image = transforms.adjust_saturation(new_image, 0.2)
-        if not os.path.exists('images'):
-            os.mkdir('images')
-        new_image.save("images/new_image{0}.jpg".format(image_list.index(x)+1))
+
+        new_image.save("images/new_image{0}.jpg".format(image_list.index(image)+1))
 
 if __name__ == "__main__":
     main()
+
+
 
