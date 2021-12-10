c80caf4892ec8674ef765f7c8921bbcac8624575
alessia_manna <60608657+alessiamanna@users.noreply.github.com>
2021-12-10 23:52:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def main():
         new_image = transforms.adjust_contrast(new_image, 10)
         new_image = transforms.adjust_saturation(new_image, 0.2)
 
-        new_image.save("images/new_image{0}.jpg".format(image_list.index(image)+1))
+        new_image.save("images/new_image{0}.jpg".format(image_list.index(image)+1), quality = 75)
 
 if __name__ == "__main__":
     main()
